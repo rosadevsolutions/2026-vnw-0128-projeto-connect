32e2f986483555dce3dfc006b52fc78f1edb1ab9
Romulo Rosa | FullStack | Rosadev Solutions <rosadevsolutions@gmail.com>
2026-01-30 18:27:08 -0300

    header com logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom"
+import logo from "../../assets/icons/logo-connect.svg"
 import "./header.module.scss"
 
 const Header = () => {
   return(
     <header>
-
+      <Link to={"/"}>
+        <img src={logo} alt="logo do projeto" />
+      </Link>
 
       <nav>
         <Link to={"/doacao"}>Doacao</Link>
@@ -12,6 +15,8 @@ const Header = () => {
         <Link to={"/mentoria"}>Mentoria</Link>
         <Link to={"/eventosep"}>Eventos & Palestras</Link>
       </nav>
+
+      <img src="https://avatars.githubusercontent.com/u/215933639?v=4" alt="Romulo Rosa" />
 
 
     </header>
